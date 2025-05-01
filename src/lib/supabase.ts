bbe8bd34b19d0392a2fcef82aa344a70ec4a19db
OsirisXxx <justtacee@gmail.com>
2025-05-01 12:08:53 +0800

    feat: Move Supabase URL and Anon Key to environment variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
 
-const supabaseUrl = 'https://hkyyzdqzdfyrvdigkrzf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhreXl6ZHF6ZGZ5cnZkaWdrcnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5OTAwODQsImV4cCI6MjA2MTU2NjA4NH0.uR09LjwB8lqc5GDpCEudWsc8TYFpw5UcIRQEHOTIEZE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+if (!supabaseUrl) {
+  throw new Error('Supabase URL is not defined');
+}
+if (!supabaseAnonKey) {
+  throw new Error('Supabase anon key is not defined');
+}
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
